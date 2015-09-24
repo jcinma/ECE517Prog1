475d1584499e7be7fbe1bf30e6718d244adbdb9f
kunal bhandari <kbhanda2@ncsu.edu>
2015-09-24 01:26:55 -0400

    changed routes to use CRUD with devise admin and user

--- lib/config/routes.rb
+++ lib/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users   scope "/admin" do
+  resources :users
+end
   resources :books
-  devise_for :admins
+  devise_for :admins  :path_prefix => 'my'
+resources :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
