b8842cb1c16de56d57eb885803a65c34e78ca019
kunal bhandari <kbhanda2@ncsu.edu>
2015-09-24 01:31:33 -0400

    generated routes for CRUD resources for admin and user

--- lib/config/routes.rb
+++ lib/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users   scope "/admin" do
+  devise_for :users  
+  scope "/admin" do
   resources :users
 end
   resources :books
-  devise_for :admins  :path_prefix => 'my'
+  devise_for :admins, :path_prefix => 'my'
 resources :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
