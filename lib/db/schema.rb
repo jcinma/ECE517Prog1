11fab0716d45f2296046062cad893044e0086505
rails <pmukher@ncsu.edu>
2015-09-24 07:12:50 +0530

    ran db:migrate for books

--- lib/db/schema.rb
+++ lib/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924011147) do
+ActiveRecord::Schema.define(version: 20150924013921) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,5 +32,16 @@ ActiveRecord::Schema.define(version: 20150924011147) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "books", force: :cascade do |t|
+    t.string   "ISBN"
+    t.string   "title"
+    t.text     "description"
+    t.text     "author"
+    t.string   "status"
+    t.string   "user"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
