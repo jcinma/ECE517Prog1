5c4d542859567dc5a641d9a0e4bd8ac688d6c395
kunal bhandari <kbhanda2@ncsu.edu>
2015-10-07 17:10:17 -0400

    modified users controller so that a user with checked out books cannot be deleted

--- lib/app/controllers/users_controller.rb
+++ lib/app/controllers/users_controller.rb
@@ -11,16 +11,22 @@ before_filter	:authenticate_admin!
   # GET /users/1.json
   def show
   end
- 
+ #books = Book.where(user_id: @user.id)
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
+  respond_to do |format|
+if Book.where(user_id: @user.id).blank?  
+  @user.destroy
+    
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
+	  else
+	        format.html { redirect_to users_url, notice: 'Cannot delete user with checked out books.' }
+      format.json { head :no_content }
     end
-  end
+end
+end
 
   private
     # Use callbacks to share common setup or constraints between actions.
