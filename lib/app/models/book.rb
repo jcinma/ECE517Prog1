d8aea523ae7a56ae5f53f871c89421c0e3d17cd0
kunal bhandari <kbhanda2@ncsu.edu>
2015-10-08 00:06:53 -0400

    adding updated model test

--- lib/app/models/book.rb
+++ lib/app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ActiveRecord::Base
-	
+validates_presence_of :ISBN
+validates_presence_of :title
+validates_presence_of :description
+validates_presence_of :author
+
+
+validates :ISBN, uniqueness: true
 	has_paper_trail on: [:update, :create], :ignore => [:title, :author, :description, :ISBN]
 
 	has_many :checkouts
