28c2f11b29eb9baff224829be420359c0bc59ef9
kunalb6 <kunal.bhandar6@gmail.com>
2015-10-09 18:15:02 +0000

    to reflect ec2 super admin

--- lib/app/controllers/admins_controller.rb
+++ lib/app/controllers/admins_controller.rb
@@ -55,7 +55,7 @@ class AdminsController < ApplicationController
   # DELETE /admins/1
   # DELETE /admins/1.json
   def destroy
-	if(@admin.id ==5 || @admin.id == current_admin.id)
+	if(@admin.id ==3 || @admin.id == current_admin.id)
     respond_to do |format|
       format.html { redirect_to admins_url, notice: 'Cannot Delete this admin.' }
       format.json { head :no_content }	
